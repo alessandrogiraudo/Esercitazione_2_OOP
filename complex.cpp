64d4a595a1196dc94c1d4fcf5066a335651ac827
Alessandro Giraudo <03alegiraudo@gmail.com>
2025-03-22 12:05:13 +0100

    modifica dei file

--- complex.cpp
+++ complex.cpp
@@ -2,9 +2,14 @@
 #include "complex.hpp"
 
 int main(void){
-	using com = complex_number<T>
-    com = coniugato(3, 5);
-    std::cout << com << std::endl;
+	using com = complex_number<int>;
+	using con = coniugato<int>
+	
+    com c(3, 5);
+	con d(3, 5);
+	
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
 
     return 0;
 }
--- complex.hpp
+++ complex.hpp
@@ -1,24 +1,30 @@
 #pragma once
+#include <concepts> 
+#include <type_traits>  // Required for std::is_arithmetic
 
-template<typename T> requires std::is_arithmetic<T> 
 
-class complex_number {
-T   re;
-T   im;
+template<typename T> requires std::is_arithmetic_v<T> 
+class complex_number 
+{
+	T   re;
+	T   im;
 
 public:
-    complex_number(){
-        re = 0;
-        im = 0;
-    };
-    complex_number(T reale, T immmaginario){
-        re = reale;
-        im = immmaginario;
-    };
+	complex_number() : re(0), im(0) {};
+	
+	complex_number(T reale, T immaginario) : re(reale), im(immaginario) {};
+    
+	/* Improve the situation of coniugato*/
+	
+	T coniuga() : re(-reale), im(-immaginario) {};
+	
+    friend std::ostream& operator<<(std::ostream& os, const complex_number& c) {
+        return os << c.re << " + " << c.im << "i";
+    }
+    friend std::ostream& operator<<(std::ostream& os, const complex_number& d) {
+        return os << d.re << " - " << d.im << "i";
+    }
 
-	complex_number_coniugato(){
-    return T complex_number(re, -im);
-};
 
 };
 
