2ce1596b8fc29bc1b3f5b8b8b4bed8e6b0bd976b
Alessandro Giraudo <03alegiraudo@gmail.com>
2025-03-24 23:47:13 +0100

    modifiche

--- complex.cpp
+++ complex.cpp
@@ -2,14 +2,18 @@
 #include "complex.hpp"
 
 int main(void){
-	using com = complex_number<int>;
-	using con = coniugato<int>
+	using com = complex_number<double>;
+	com c1(1, 2);
+	com c2(1, -2);
 	
-    com c(3, 5);
-	con d(3, 5);
+    std::cout << c1 << std::endl;
+    std::cout << c2 << std::endl;
+	std::cout << c2.coniugato() << std::endl;
+	com c3 = c1 += c2;
+	com c4 = c1 + c2;
+	std::cout << c3 << std::endl;
+	std::cout << c4 << std::endl;
 	
-    std::cout << c << std::endl;
-    std::cout << d << std::endl;
 
     return 0;
-}
+};
--- complex.hpp
+++ complex.hpp
@@ -3,7 +3,8 @@
 #include <type_traits>  // Required for std::is_arithmetic
 
 
-template<typename T> requires std::is_arithmetic_v<T> 
+template<typename T> 
+requires std::floating_point<T> 
 class complex_number 
 {
 	T   re;
@@ -16,17 +17,45 @@ public:
     
 	/* Improve the situation of coniugato*/
 	
-	T coniuga() : re(-reale), im(-immaginario) {};
+	T real() const {return re;}
+	T imag() const {return im;}	
+	complex_number coniugato() const {
+		return complex_number(re, -im);
+	}
 	
-    friend std::ostream& operator<<(std::ostream& os, const complex_number& c) {
-        return os << c.re << " + " << c.im << "i";
-    }
-    friend std::ostream& operator<<(std::ostream& os, const complex_number& d) {
-        return os << d.re << " - " << d.im << "i";
-    }
-
+	complex_number& operator+=(const complex_number<T>& other) {
+		re += other.re;
+		im += other.im;
+		return *this;
+	}
+	
+	complex_number& operator+(const complex_number<T>& other) const {
+		//return complex_number com = *this;
+		complex_number<T> com = *this;
+		com += other;
+		return complex_number<T> com;
+	}
+	
+	
+	complex_number& operator*=(const complex_number<T>& other) {
+		T real = re * other.re - im * other.im;
+		T imma = re * other.im + im * other.re;
+		re = real;
+		im = imma;
+		return *this;
+	}
 
 };
+template<typename T>
+std::ostream&
+operator<<(std::ostream& os, const complex_number<T>& c) {
+	 if (c.imag() >= 0) {
+        return os << c.real() << " + " << c.imag() << "i";
+	 } else{
+		return os << c.real() << c.imag() << "i";
+	};
+    };
+
 
 
 
